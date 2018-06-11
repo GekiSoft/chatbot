e9bd8850ad56d6efd5dd9a1168ab9acb568e7b87
GekiSoft <GekiSoft@gmail.com>
2018-06-11 14:42:38 +0800

    edit typo

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function callSendAPI(sender_psid, response) {
 
     request({
         "uri" : "https://graph.facebook.com/v2.6/me/messages",
-        "qs" : { "acces_token" : PAGE_ACCESS_TOKEN },
+        "qs" : { "access_token" : PAGE_ACCESS_TOKEN },
         "method" : "POST",
         "json" : request_body
     }, (err, res, body) => {
