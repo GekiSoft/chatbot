54d450e505ecbef0ea18d1d8717b59a2de7d31fa
GekiSoft <GekiSoft@gmail.com>
2018-06-11 13:37:48 +0800

    pushing 3

--- index.js
+++ index.js
@@ -28,17 +28,20 @@ app.get('/webhook/', function(req, res) {
 
 let PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
 
-app.post('/webhook/', function(req, res) {
-    let messaging_events = req.body.entry.messaging[0]
-    for (let i = 0; i< messaging_events.length; i++) {
-        let event = messaging_events[i]
-        let sender = event.sender.id
-        if(event.message && event.message.text) {
-            let text = event.message.text
-            sendText(sender, "Text echo: " + text.substring(0, 100))
-        }
+app.post('/webhook/', (req, res) => {
+    let body = req.body
+
+    if (body.object === 'page') {
+        body.entry.forEach(function (entry) {
+            let webhook_event = entry.messaging[0]
+            console.log(webhook_event)
+            let sender_psid = webhook_event.sender.id
+            console.log('Sender PSID: ' + sender_psid)
+        })
+        res.status(200).send('EVENT_RECEIVED')
+    } else {
+        res.sendStatus(404)
     }
-    res.sendStatus(200)
 })
 
 function sendText(sender, text) {
