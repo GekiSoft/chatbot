f42c2c0238401ac1df37e3ca51bc245340d8c657
GekiSoft <GekiSoft@gmail.com>
2018-06-11 13:47:34 +0800

    push pa more

--- index.js
+++ index.js
@@ -28,19 +28,20 @@ app.get('/webhook/', function(req, res) {
 
 let PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
 
+
 app.post('/webhook/', (req, res) => {
-    let body = req.body
+    let body = req.body;
 
     if (body.object === 'page') {
         body.entry.forEach(function (entry) {
-            let webhook_event = entry.messaging[0]
-            console.log(webhook_event)
-            let sender_psid = webhook_event.sender.id
-            console.log('Sender PSID: ' + sender_psid)
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
         })
-        res.status(200).send('EVENT_RECEIVED')
+        res.status(200).send('EVENT_RECEIVED');
     } else {
-        res.sendStatus(404)
+        res.sendStatus(404);
     }
 })
 
